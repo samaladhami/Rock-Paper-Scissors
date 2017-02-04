e37098b5b67571e7f2b3def8768114bd9e948fb8
samer <samaladhami@gmail.com>
2017-02-04 16:45:34 -0600

    added result template

--- js/gameOver.js
+++ js/gameOver.js
@@ -2,16 +2,30 @@
 let gameOver;
 //
 $( document ).ready(function(){
+  let resultText;
+  let resultTextColor;
   gameOver = () => {   // stop the game and show the score
     $('button').prop( 'disabled' , true ); // disable all buttons
-    $( '#gameOver' ).html(
+    getResult();
+    $( '#gameOver' ).html( // wins losses and draws from playEngine.js
       `<div></div>
-      <h1>You win!!</h1>
-      <h2>Wins: 0</h2>
-      <h2>Losses: 0</h2>
-      <h2>Draws: 0</h2>
-      <button type="button">Play again</button>
+      <h1 style='color: ${ resultTextColor }'>${ resultText }</h1>
+      <h2>Wins: ${ wins }</h2>
+      <h2>Losses: ${ losses }</h2>
+      <h2>Draws: ${ draws }</h2>
+      <button type="button" onclick='location.reload()'>Play again</button>
       `
     )
+  }
+
+  function getResult(){
+    if( wins > losses ){
+      resultText = 'You win!!';
+      resultTextColor = '#77d673'
+    }
+    else {
+      resultText = 'You lose!'
+      resultTextColor = '#e84040'
+    }
   }
 })
