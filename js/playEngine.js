a8613c9c43094b23382a1c5aa7654f77ee694bd5
samer <samaladhami@gmail.com>
2017-02-03 18:44:42 -0600

    fliped user img

--- js/playEngine.js
+++ js/playEngine.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
+  let userChoice;
+  let botChoice;
+  let userTurn = true;
+  const playArray = [ 'rock' , 'paper' , 'scissors' ]
 
   $('#rock-btn').click( () => {
+    userTurn = false;
+    userChoice = 'rock';
     $('#user').empty();
     $('#user').append('<img src="imgs/rock.png" alt="rock" />')
   })
   $('#paper-btn').click( () => {
+    userTurn = false;
+    userChoice = 'paper';
     $('#user').empty();
     $('#user').append('<img src="imgs/paper.png" alt="paper" />')
   })
   $('#scissors-btn').click( () => {
+    userTurn = false;
+    userChoice = 'scissors';
     $('#user').empty();
     $('#user').append('<img src="imgs/scissors.png" alt="scissors" />')
-  })
-  
+  });
+
+  setInterval( ( ) => {
+    if(!userTurn) {
+      botChoice = playArray[ Math.floor( Math.random( ) * playArray.length ) ];
+      $('#bot').empty();
+      $('#bot').append(`<img src="imgs/${ botChoice }.png" alt="rock" />`);
+      userTurn = true;
+    }
+  } , 0)
+
 })
